3186a5a524470be3fc2678c568b411f79476fd4c
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-13 17:19:28 -0400

    Update 71480482338212.lua

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -5,9 +5,10 @@
 
     game: BedwarZ (3)
 ]]
-
+print("71480482338212.lua")
 local Pineapple = loadstring(readfile('pineapple/gui/pineapple.lua'))()
 
 Pineapple:CreateMain()
 
 Pineapple:CreateTab({Combat, 1, 255, 255, 255})
+print("it loaded")
