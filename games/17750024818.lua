50e4c42cc57cd7d71964ffb625efb46e10316c8b
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-13 17:24:24 -0400

    Update 17750024818.lua

--- games/17750024818.lua
+++ games/17750024818.lua
@@ -7,5 +7,16 @@
 ]]
 
 local Pineapple = loadstring(readfile('pineapple/gui/pineapple.lua'))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingChairV4/pineapple/refs/heads/main/gui/pineapple.lua"))()
 
-Pineapple:CreateMain()
+local MainUI = Library:CreateMain({
+	TextCharacters = 10,
+	Toggle = "RightShift",
+	
+})
+
+local tab1 = Library:CreateTab({
+	Text = "Tab1",
+	Image = "rbxassetid://0",
+	ImageColor = Color3.fromRGB(0,0,0)
+})
