4903777ed6de612868cd0933996bd30ba81b7e5d
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-13 17:25:03 -0400

    Update 17750024818.lua

--- games/17750024818.lua
+++ games/17750024818.lua
@@ -20,3 +20,21 @@ local tab1 = Library:CreateTab({
 	Image = "rbxassetid://0",
 	ImageColor = Color3.fromRGB(0,0,0)
 })
+
+local toggle1 = tab1:CreateToggle({
+	Name = "Name of the toggle",
+	ToolTipText = "Tool Tip Text of the toggle",
+	Keybind = "The keybind Default: None",
+	Enabled = Enabled by defualt?: Bool true / false,
+	AutoDisable = Disable by Automatically? Bool true / false,
+	AutoEnable = Enable by Automatically? Bool true / false,
+	Hide = Hide the toggle? "Visiblity = false" Bool true / false,
+	Callback = function(callback)
+	-- callback is a bool if the toggle is toggled true / false ---
+	if callback then
+	print("Active")
+	else
+	print("Not active")
+		end
+	end,
+})
