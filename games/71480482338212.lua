31dbb34b12565b1337f6fec99e1470d7d2b2c53a
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-13 17:45:52 -0400

    Update 71480482338212.lua

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -1,14 +1,42 @@
 --[[
 
     pineapple 🍍
-    by @stav, @sus, @GamingChairV4, @DaiPlayz, @cqrzy, @EZHubBot
+    by @stav, @sus, @GamingChairV4, @DaiPlayz, @cqrzy, @star
 
-    game: BedwarZ (3)
+    game: Bedwarz
 ]]
-print("71480482338212.lua")
+
 local Pineapple = loadstring(readfile('pineapple/gui/pineapple.lua'))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingChairV4/pineapple/refs/heads/main/gui/pineapple.lua"))()
 
-Pineapple:CreateMain()
+local MainUI = Library:CreateMain({
+	TextCharacters = 10,
+	Toggle = "RightShift",
+	
+})
 
-Pineapple:CreateTab({Combat, 1, 255, 255, 255})
-print("it loaded")
+local Combat = Library:CreateTab({
+	Text = "Combat",
+	Image = "rbxassetid://0",
+	ImageColor = Color3.fromRGB(0,0,0)
+})
+
+local toggle1 = tab1:CreateToggle({
+	Name = "Name of the toggle",
+	ToolTipText = "Tool Tip Text of the toggle",
+	Keybind = "The keybind Default: None",
+	Enabled = true,
+	AutoDisable = false,
+	AutoEnable = false,
+	Hide = false,
+	Callback = function(callback)
+	-- callback is a bool if the toggle is toggled true / false ---
+	if callback then
+	print("Active")
+    Library:notif("Pineapple","Active", 15, "info")
+	else
+	print("Not active")
+        Library:notif("Pineapple","Not active", 3, "info")
+		end
+	end,
+})
