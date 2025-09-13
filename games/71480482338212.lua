50d0fc82693feb65e6f1896b0231c94e0ce49726
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-13 19:15:45 -0400

    Update 71480482338212.lua

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -9,66 +9,21 @@
 local Pineapple = loadstring(readfile('pineapple/gui/pineapple.lua'))()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingChairV4/pineapple/refs/heads/main/gui/pineapple.lua"))()
 
-local main = Pineapple:CreateMain({
-    textCharacters = 10,
+local MainUI = Library:CreateMain({
+    TextCharacters = 10,
     Toggle = "RightShift",
+
 })
 
-local tabs = {
-    Combat = main:CreateTab({
-        Text = 'Combat',
-        Icon = 'rbxassetid://138185990548352',
-        ImageColor = Color3.fromRGB(255, 255, 255)
-    }),
-    Exploit = main:CreateTab({
-        Text = 'Exploit',
-        Icon = 'rbxassetid://71954798465945',
-        ImageColor = Color3.fromRGB(255, 255, 255)
-    }),
-    Move = main :CreateTab({
-        Text = 'Move',
-        Icon = 'rbxassetid://91366694317593',
-        ImageColor = Color3.fromRGB(255, 255, 255)
-    }),
-    Player = main:CreateTab({
-        Text = 'Player',
-        Icon = 'rbxassetid://103157697311305',
-        ImageColor = Color3.fromRGB(255, 255, 255)
-    }),
-    Visual = main:CreateTab({
-        Text = 'Visual',
-        Icon = 'rbxassetid://118420030502964',
-        ImageColor = Color3.fromRGB(255, 255, 255)
-    }),
-    World = main:CreateTab({
-        Text = 'World',
-        Icon = 'rbxassetid://76313147188124',
-        ImageColor = Color3.fromRGB(255, 0, 0)
-    })
-}
-
-do
-    local Speed
-    Speed = tabs.Move:CreateToggle({
-        Name = 'Speed',
-        Function = function(callback)
-            if callback then
-                repeat
-                    lplr.Character.Humanoid.WalkSpeed = 23
-                    task.wait()
-                until not callback
-            else
-                lplr.Character.Humanoid.WalkSpeed = 16
-            end
-        end,
-        ToolTipText = 'Increases your speed'
-    })
-end
-
-Library:notif("Pineapple","Executed properly!", 3, "info")
+local Combat = MainUI:CreateTab({
+    Text = "Combat",
+    Image = "rbxassetid://138185990548352",
+    ImageColor = Color3.fromRGB(0,0,0)
+})
+        Library:notif("Pineapple","Executed properly!", 3, "info")
 
 
-local Killaura = tabs.Combat:CreateToggle({
+local Killaura = Combat:CreateToggle({
     Name = "Killaura",
     ToolTipText = "Made possible by @stav",
     Keybind = "None",
@@ -88,7 +43,7 @@ local Killaura = tabs.Combat:CreateToggle({
     end,
 })
 
-local Uninject = tabs.Player:CreateToggle({
+local Uninject = Combat:CreateToggle({
     Name = "Uninject",
     ToolTipText = "Uninject pineapple from your client.",
     Keybind = "None",
