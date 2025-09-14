8498c21798d4f382d7894c7bcc01f30fa5cc9618
GamingChairV4 <126362292+GamingChairV4@users.noreply.github.com>
2025-09-14 11:05:00 -0400

    Added all tabs

--- games/71480482338212.lua
+++ games/71480482338212.lua
@@ -20,10 +20,36 @@ local Combat = MainUI:CreateTab({
     Image = "rbxassetid://138185990548352",
     ImageColor = Color3.fromRGB(0,0,0)
 })
+
+local Blatant = MainUI:CreateTab({
+    Text = "Blatant",
+    Image = "rbxassetid://138185990548352",
+    ImageColor = Color3.fromRGB(0,0,0)
+})
+
+local Player = MainUI:CreateTab({
+    Text = "Player",
+    Image = "rbxassetid://138185990548352",
+    ImageColor = Color3.fromRGB(0,0,0)
+})
+
+local Movement = MainUI:CreateTab({
+    Text = "Movement",
+    Image = "rbxassetid://138185990548352",
+    ImageColor = Color3.fromRGB(0,0,0)
+})
+
+local Utility = MainUI:CreateTab({
+    Text = "Utility",
+    Image = "rbxassetid://138185990548352",
+    ImageColor = Color3.fromRGB(0,0,0)
+})
         Library:notif("Pineapple","Executed properly!", 3, "info")
+        Library:notif("Pineapple","This script is in BETA & some functions are broken.", 15, "info")
 
 
-local Killaura = Combat:CreateToggle({
+
+local Killaura = Utility:CreateToggle({
     Name = "Killaura",
     ToolTipText = "Made possible by @stav",
     Keybind = "None",
@@ -35,7 +61,7 @@ local Killaura = Combat:CreateToggle({
     -- callback is a bool if the toggle is toggled true / false ---
     if callback then
     print("Active")
-        Library:notif("Pineapple","Enabled Killaura", 3, "info")
+        Library:notif("Pineapple","Enabled killaura", 3, "info")
     else
     print("Not active")
         Library:notif("Pineapple","Disabled Killaura", 3, "info")
@@ -43,7 +69,7 @@ local Killaura = Combat:CreateToggle({
     end,
 })
 
-local Uninject = Combat:CreateToggle({
+local Uninject = Utility:CreateToggle({
     Name = "Uninject",
     ToolTipText = "Uninject pineapple from your client.",
     Keybind = "None",
